2ebae1be21a51581cb4baae2d011e7494f77aeaf
Ezhik <i@ezhik.me>
2017-09-18 00:39:52 -0400

    Initial Commit

---
+++ NotchApp/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  NotchApp
+//
+//  Created by Ezhik on 9/18/17.
+//
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ NotchApp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  NotchApp
+//
+//  Created by Ezhik on 9/18/17.
+//
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
